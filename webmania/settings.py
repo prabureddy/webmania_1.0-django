2afd8655825f3252bdcc2be7af559b02f03d8c40
prabureddy <prabu0reddy777@gmail.com>
2019-10-05 23:33:16 +0530

    changed password

--- webmania/settings.py
+++ webmania/settings.py
@@ -52,8 +52,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "prabu0reddy777@gmail.com"
-EMAIL_HOST_PASSWORD = "lhkuxwalobondynd"
+EMAIL_HOST_USER = "Email Id"
+EMAIL_HOST_PASSWORD = "Password"
 ADMIN_EMAIL = [
     'prabu0reddy777@gmail.com',
 ]
